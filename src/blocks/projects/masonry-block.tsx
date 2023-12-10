fddb81c25441f8d83c2471fd1f693ffce86029ed
Connor Doman <cdoman@nowmediagroup.ca>
2023-12-09 20:15:32 -0800

    feat(styles): added masonry stylesheet

--- src/blocks/projects/masonry-block.tsx
+++ src/blocks/projects/masonry-block.tsx
@@ -20,6 +20,9 @@ export default class ProjectsMasonryBlock {
 
     projectsData: any[];
 
+    brickWidth: number;
+    brickHeight: number;
+
     constructor(block: HTMLElement) {
         this.block = block;
         const kernel = block.querySelector(".masonry-kernel") as HTMLElement;
@@ -39,8 +42,21 @@ export default class ProjectsMasonryBlock {
     }
 
     checkOverlap(div1: HTMLElement, div2: HTMLElement): boolean {
-        const rect1 = div1.getBoundingClientRect();
-        const rect2 = div2.getBoundingClientRect();
+        const rect1 = {
+            left: parseInt(div1.style.left),
+            top: parseInt(div1.style.top),
+            right: parseInt(div1.style.left) + this.brickWidth,
+            bottom: parseInt(div1.style.top) + this.brickHeight,
+        };
+
+        const rect2 = {
+            left: parseInt(div2.style.left),
+            top: parseInt(div2.style.top),
+            right: parseInt(div2.style.left) + this.brickWidth,
+            bottom: parseInt(div2.style.top) + this.brickHeight,
+        };
+
+        console.info("Checking overlap for:", { w: this.brickWidth, h: this.brickHeight, rect1, rect2 });
 
         const overlap = !(
             rect1.right < rect2.left ||
@@ -56,14 +72,23 @@ export default class ProjectsMasonryBlock {
         const kernelTop = this.masonryKernel.offsetTop;
         const kernelLeft = this.masonryKernel.offsetLeft;
 
+        console.log("Brick width:", getComputedStyle(this.block).getPropertyValue("--brick-width"));
+
+        this.brickWidth = parseFloat(getComputedStyle(this.block).getPropertyValue("--brick-width"));
+        this.brickHeight = parseFloat(getComputedStyle(this.block).getPropertyValue("--brick-width"));
+
         const divs: HTMLElement[] = [];
         this.projectsData.forEach((project: any) => {
             const brick = document.createElement("div");
             brick.classList.add("brick");
+            brick.style.width = this.brickWidth + "px";
+            brick.style.height = this.brickHeight + "px";
 
-            while (true) {
+            let tries = 0;
+
+            while (tries < 20) {
                 const angle = Math.random() * 2 * Math.PI;
-                const distance = Math.random() * 200;
+                const distance = Math.random() * this.brickWidth + this.brickWidth;
 
                 const x = kernelTop + Math.cos(angle) * distance;
                 const y = kernelLeft + Math.sin(angle) * distance;
@@ -76,6 +101,7 @@ export default class ProjectsMasonryBlock {
                     break;
                 }
                 console.warn("New div overlapping, trying again...");
+                tries++;
             }
 
             this.block.appendChild(brick);
@@ -97,7 +123,7 @@ export default class ProjectsMasonryBlock {
 
         return (
             <section {...blockProps}>
-                <div className="masonry-kernel"></div>
+                <div className="masonry-kernel">PROJECTS</div>
             </section>
         );
     };
